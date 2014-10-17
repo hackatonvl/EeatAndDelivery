1c2ac4a6ecfe6a85f6e0ba1c7ae0d9b3e46f28e3
biopsihoz <biopsihoz@hotmail.com>
2014-10-17 21:58:35 +1100

    Product and Store add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hackaton::Application.config.secret_key_base = '02a204d5de80ea05a37db48a2f21fa1a16a035fd2ae869ab0bc56e691c47a81e81dd2d545fa27f4fbeaa7f30991840d1fecdfcfc8fc506f453b2a348b230749c'
