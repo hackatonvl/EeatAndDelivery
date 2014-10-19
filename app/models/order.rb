760b6ca8b0e6966ed5984998e724726cdc76037d
biopsihoz <biopsihoz@hotmail.com>
2014-10-19 17:34:48 +1100

    some validates to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,9 @@ class Order < ActiveRecord::Base
   
   PAYMENT_TYPES = [ "ShutUpAndTakeMyMoney" ]
 
+	validates :name, :address, :email, presence: true
+	validates :pay_type, inclusion: PAYMENT_TYPES
+
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
